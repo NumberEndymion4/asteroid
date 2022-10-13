f1c4105ea6aafa02fc640223da66fca8dfa3cc84
Artem Popov <popov-89@hotmail.com>
2022-10-13 21:02:00 +0200

    Create empty MonoGame project

---
+++ Asteroid/src/GameApp.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Asteroid
+{
+	public class GameApp : Game
+	{
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+
+		public GameApp()
+		{
+			graphics = new GraphicsDeviceManager(this);
+
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (Keyboard.GetState().IsKeyDown(Keys.Escape)) {
+				Exit();
+			}
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ Asteroid/src/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Asteroid
+{
+	public static class Program
+	{
+		static int Main(string[] args)
+		{
+			using var game = new GameApp();
+			game.Run();
+			return 0;
+		}
+	}
+}
